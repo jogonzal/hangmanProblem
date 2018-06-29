7c08f8ecc19642362873d1c1ec97d40a997dfa23
Jorge <jogonzal@users.noreply.github.com>
2018-06-28 20:55:58 -0700

    remove dead code

--- src/components/App.tsx
+++ src/components/App.tsx
@@ -4,10 +4,15 @@ import {
   Route,
   Link
 } from 'react-router-dom'
+const reactLogo = require('./../assets/img/react_logo.svg')
+import './../assets/scss/App.scss'
 
 const Home = () => (
   <div>
     <h2>Home</h2>
+    <h1>Hello World!</h1>
+    <p>Foo to the barz</p>
+    <img src={reactLogo} height='480'/>
   </div>
 )
 
@@ -69,24 +74,3 @@ const BasicExample = () => (
   </Router>
 )
 export default BasicExample
-
-
-// import * as React from 'react';
-// import './../assets/scss/App.scss';
-
-// const reactLogo = require('./../assets/img/react_logo.svg');
-
-// export interface AppProps {
-// }
-
-// export default class App extends React.Component<AppProps, undefined> {
-//     render() {
-//         return (
-//             <div className='app'>
-//                 <h1>Hello World!</h1>
-//                 <p>Foo to the barz</p>
-//                 <img src={reactLogo} height='480'/>
-//             </div>
-//         );
-//     }
-// }
