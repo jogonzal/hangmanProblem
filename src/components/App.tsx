f7aee66951bc46ac966764d75b9bf943a8c780a1
Jorge <jogonzal@users.noreply.github.com>
2018-06-30 17:15:51 -0700

    More react samples

--- src/components/App.tsx
+++ src/components/App.tsx
@@ -1,12 +1,18 @@
 import * as React from 'react'
 const reactLogo = require('./../assets/img/react_logo.svg')
 import './../assets/scss/App.scss'
+import Clock from './Clock/Clock'
+import Toggle from './Toggle'
 
 const App = () => (
   <div className='app'>
       <h1>Hello World!</h1>
       <p>Foo to the barz</p>
       <img src={reactLogo} height='480'/>
+      <Clock input={ 'JORGE CLOCK 1' }/>
+      <Clock input={ 'JORGE CLOCK 2' }/>
+      <Clock input={ 'JORGE CLOCK 3' }/>
+      <Toggle />
   </div>
 )
 
