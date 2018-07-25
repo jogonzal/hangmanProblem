749179b76efaadee105c31bb015b4a622b081a75
Jorge <jogonzal@users.noreply.github.com>
2018-07-24 22:46:20 -0700

    more comments

--- src/index.tsx
+++ src/index.tsx
@@ -5,15 +5,20 @@ import HangmanGamePlayer from './HangmanGamePlayer'
 console.log('Game starting...')
 
 async function startApp() {
+  // Read the words JSON
   const response = await fetch(fiveLetterWordsFilePath)
   const fiveLetterWords: string[] = await response.json()
 
   console.log(`There are a total of ${fiveLetterWords.length} words in the pool`)
 
+  // Initialize game classes
   const game = new HangmanGameManager(fiveLetterWords)
-  const player = new HangmanGamePlayer(game)
+  const player = new HangmanGamePlayer(game, fiveLetterWords)
 
+  // Play the game
   player.playGame()
+
+  // TODO: Add evaluation 
 }
 
 startApp()
